d7776aefc50049ab75ef3755455e9d3edd518c80
Paulo Sergio <pscosta2008@gmail.com>
2024-10-19 18:07:52 -0300

    feat: adding logic to open github app in CreditosPage

--- Deutschland-Game/View/CreditosPage.xaml.cs
+++ Deutschland-Game/View/CreditosPage.xaml.cs
@@ -6,4 +6,52 @@ public partial class CreditosPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void gitHubImageBtn_Clicked(object sender, EventArgs e)
+    {
+
+        modalSection.IsVisible = true;
+
+    }
+
+    private async void backIconBtn_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+
+    private void backendBtn_Clicked(object sender, EventArgs e)
+    {
+        Uri uri = new Uri("https://github.com/EduardoPQueiroz/deutschland_game_project");
+        tryToOpenGitHub(uri);
+    }
+
+    private void frontendBtn_Clicked(object sender, EventArgs e)
+    {
+        Uri uri = new Uri("https://github.com/danielmarinhom/deutschland-maui");
+        tryToOpenGitHub(uri);
+    }
+
+    private void closeModalBtn_Clicked(object sender, EventArgs e)
+    {
+        modalSection.IsVisible = false;
+    }
+
+    public async Task<bool> canOpenGitHubApp(Uri uri)
+    {
+        return await Launcher.TryOpenAsync(uri);
+    }
+
+    public async void openGitHubWebSite(Uri uri)
+    {
+        await Browser.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+    }
+
+    public void tryToOpenGitHub(Uri uri)
+    {
+        bool gitHubAppOpened = canOpenGitHubApp(uri).Result;
+        if (!gitHubAppOpened)
+        {
+            openGitHubWebSite(uri);
+        }
+    }
 }
