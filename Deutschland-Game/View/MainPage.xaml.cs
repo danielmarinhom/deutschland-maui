ce4813ec8d368aaec4546d1cbb3886dc22fd0787
Paulo Sergio <pscosta2008@gmail.com>
2024-10-19 18:31:51 -0300

    fix: fixing some merges

--- Deutschland-Game/View/MainPage.xaml.cs
+++ Deutschland-Game/View/MainPage.xaml.cs
@@ -8,22 +8,17 @@ namespace Deutschland_Game
         public MainPage()
         {
             InitializeComponent();
-
-            
         }
 
         private async void creditosBtn_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new CreditosPage());
         }
-<<<<<<< HEAD
 
         private async void jogarBtn_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new EscolherNomePage());
         }
-=======
->>>>>>> a6e09fecbdb0c67935aa73cc8aeb02177bfa0a10
     }
 
 }
